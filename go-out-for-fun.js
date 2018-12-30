733e63016ebdba9d71c97b659dcafd2662e9042b
roxas <roxas@landinsight.io>
2018-12-30 20:44:48 +0100

    the loop

--- go-out-for-fun.js
+++ go-out-for-fun.js
@@ -1,12 +1,31 @@
 /* 
 Password Check function
-*/
 
-Check = prompt('Enter password', ''); 
-while (Check != 'Tidi') {
-    Check = prompt('Enter password', '');
-}
- {
+
+Check = prompt('Enter password', '');
+if (Check != 'Tidi') {
+    for (i, i < 10, i++) {
+        Check = prompt('Enter password', '');
+    }
+else
+    {
         alert('Well done');
+    }
+
+    */
+
+for (i = 1; i <= 4; i++) {
+    Check = prompt('Enter Password');
+    if (i==4) {
+        window.close();
+    }
+    if (Check != 'Tidi') {
+        alert('Try Again');
+    }
+    else {
+        alert('well done');
+        break;
+    }
+    
     }
 
